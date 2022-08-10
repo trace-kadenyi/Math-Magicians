d1b68374f5d2fa96edbabea95e5df3de6b44e6b3
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-10 14:22:56 +0200

    add tests for operate.js

---
+++ src/Logic/__test__/operate.test.js
@@ -0,0 +1,16 @@
+import operate from "../operate";
+
+describe('operations', () => {
+  test('it adds numbers', () => {
+    expect(operate(2, 2, '+')).toBe('4')
+  })
+  test('it subtracts numbers', () => {
+    expect(operate(2, 2, '-')).toBe('0')
+  })
+  test('it multiplies numbers', () => {
+    expect(operate(2, 2, 'x')).toBe('4')
+  })
+  test('it divides numbers', () => {
+    expect(operate(2, 2, '÷')).toBe('1')
+  })
+})
