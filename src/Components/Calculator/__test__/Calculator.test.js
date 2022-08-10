b5d740bed697c3280571dd8caf65125502e8e1e4
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-10 16:08:00 +0200

    test for user interaction changing the dom

--- src/Components/Calculator/__test__/Calculator.test.js
+++ src/Components/Calculator/__test__/Calculator.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getAllByText, getByText, render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Calculator from '../Calculator';
 
 test('should render calculator', () => {
   const calc = render(<Router><Calculator /></Router>)
+  screen.debug()
   expect(calc).toMatchSnapshot();
 });
 
-// test('screen updates when user clicks a number', () => {
-//   render(<Router><Calculator /></Router>)
-// })
+test('screen updates when user clicks a number', () => {
+  render(<Router><Calculator /></Router>)
+  expect(screen.getByText('2')).toBeInTheDocument();
+  const button2 = screen.getByText('2')
+  const button5 = screen.getByText('5')
+  fireEvent.click(button2)
+  fireEvent.click(button5)
+  const display = screen.getByText('25')
+  screen.debug()
+  expect(display).toBeInTheDocument()
+
+})
