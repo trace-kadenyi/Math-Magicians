8eb21f318829066761778b8620b3f86df98d66b9
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-10 14:28:08 +0200

    add tests for calculate.js

---
+++ src/Logic/__test__/calculate.test.js
@@ -0,0 +1,33 @@
+import calculate from "../calculate";
+
+describe('calculations', () => {
+  test('it sets the next number', () => {
+    let obj = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+
+    expect(calculate(obj, '2').next).toBe('2')
+  })
+
+  test('it sets to total from an operation', () => {
+    let obj = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+
+    obj = calculate(obj, '2')
+    expect(obj.next).toBe('2')
+
+    obj = calculate(obj, '+')
+    expect(obj.operation).toBe('+')
+
+    obj = calculate(obj, '2')
+    expect(obj.total).toBe('2')
+
+    obj = calculate(obj, '=')
+    expect(obj.total).toBe('4')
+  })
+})
